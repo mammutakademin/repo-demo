ca11a2877852f84c0223fb9ca433fac017b92ab4
Editha Trinitaria <edithtrinitaria@gmail.com>
2021-09-24 10:27:42 +0200

    Added more changes

--- javascript-demo/minimumOfTwo/main.js
+++ javascript-demo/minimumOfTwo/main.js
@@ -20,10 +20,30 @@ function minOfThree(num1, num2, num3) {
     return result
 }
 
+function sumTo(num) {
+    let sum = 0;
+    let i = 0;
+    while(i < num) {
+        i = i + 1;
+        sum = sum + i;
+    }
+    return sum;
+}
+
+// function sumTo(num) {
+//     let sum = 0;
+//     for(let i = 0; i <= num; i = i + 1) {
+//         sum = sum + 1;
+//     }
+//     return sum;
+// }
+
 function main() {
     let min1 = minimumOfTwo(8, 2);
     let min2 = minimumOfTwo(9, 6);
     let min3 = minOfThree(9, 5, 2);
+    let sum1 = sumTo(5);
+    let sum2 = sumTo(8);
 }
 
 main();
