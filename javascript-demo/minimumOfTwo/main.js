4a7acf2be521b3419615a33ed2a9cda6fde6ab80
Editha Trinitaria <edithtrinitaria@gmail.com>
2021-09-24 12:13:56 +0200

    Added fibonacci function

--- javascript-demo/minimumOfTwo/main.js
+++ javascript-demo/minimumOfTwo/main.js
@@ -8,16 +8,28 @@ function minimumOfTwo(num1, num2) {
     return result;
 }
 
+// function minOfThree(num1, num2, num3) {
+//     let result;
+//     let min = minimumOfTwo(num1, num2);
+//     if (min > num3) {
+//         result = num3;
+//     }else {
+//         result = min;
+//     }
+//     return result
+// }
 
-function minOfThree(num1, num2, num3) {
-    let result;
-    let min = minimumOfTwo(num1, num2);
-    if (min > num3) {
-        result = num3;
+function minimumOfThree(num1, num2, num3) {
+    let min = 0;
+    if (num1 > num2) {
+        min = num2;
+        if (num2 > num3) {
+            min = num3;
+        }
     }else {
-        result = min;
+        min = num1;
     }
-    return result
+    return min;
 }
 
 // function sumTo(num) {
@@ -29,14 +41,6 @@ function minOfThree(num1, num2, num3) {
 //     }
 //     return sum;
 // }
-
-function sumTo(num) {
-    let sum = 0;
-    for(let i = 0; i <= num; i = i + 1) {
-        sum = sum + i;
-    }
-    return sum;
-}
 
 function ticketMachine(age) {
     let price = 0;
@@ -50,15 +54,40 @@ function ticketMachine(age) {
     return price;
 }
 
+function sumTo(num) {
+    let sum = 0;
+    for(let i = 0; i <= num; i = i + 1) {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+function fibonacci(input) {
+    let a = 1;
+    let b = 0;
+    let output = 0;
+    let i = 0;
+    while(i < input) {
+        output = a + b;
+        a = b;
+        b = output;
+        i = i + 1;
+    }
+    return output;
+}
+
 function main() {
     let min1 = minimumOfTwo(8, 2);
     let min2 = minimumOfTwo(9, 6);
-    let min3 = minOfThree(9, 5, 2);
-    let sum1 = sumTo(5);
-    let sum2 = sumTo(8);
+    let min3 = minOfThree(2, 5, 9);
     let price1 = ticketMachine(4);
     let price2 = ticketMachine(48);
     let price3 = ticketMachine(99);
+    let sum1 = sumTo(5);
+    let sum2 = sumTo(6);
+    let output1 = fibonacci(10);
+    let output2 = fibonacci(3);
+    let output3 = fibonacci(1);   
 }
 
 main();
