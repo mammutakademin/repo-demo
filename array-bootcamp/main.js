d6e2de473530839208ad5527e33f646c83b47b75
Editha Trinitaria <edithtrinitaria@gmail.com>
2021-10-04 14:48:35 +0200

    bootcamp

---
+++ array-bootcamp/main.js
@@ -0,0 +1,282 @@
+function containsElement(arr, element) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] == element) {
+            return true
+
+        }
+    }
+    return false
+}
+
+function main() {
+    let arr = [1, 7, 1, 0, 5]
+    let element = 8
+    let result = containsElement(arr, element)
+}
+main()
+
+/*function indexOfElement(arr, element) {
+    let output = -1
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] == element) {
+            return i
+        }
+    }
+    return output
+}
+
+function main() {
+    let arr = [1, 7, 1, 0, 5]
+    let element = 8
+    let result = indexOfElement(arr, element)
+}
+main()*/
+
+/*function min(arr) {
+    let minimum = arr[0]
+    for (let i = 1; i < arr.length; i++) {
+        if (minimum > arr[i]) {
+            minimum = arr[i]
+        }
+    }
+    return minimum
+}
+
+function main() {
+    let arr = [1, 7, 1, 0, 5, ]
+    let result = min(arr)
+}
+main()*/
+
+/*function max(arr) {
+    let maximum = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if (maximum < arr[i]) {
+            maximum = arr[i]
+        }
+    }
+    return maximum
+}
+
+function main() {
+    let arr = [1, 7, 1, 0, 5]
+    let result = max(arr)
+}
+main()*/
+
+/*function sum(arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+            //if (sum > arr[i]) {
+            //sum = arr[i]
+        //}
+    }
+    return sum
+}
+
+function main() {
+    let arr = [1, 7, 1, 0, 5, ]
+    let result = sum(arr)
+}
+main()*/
+
+/*function mean(arr) {
+    let medel = 0
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    medel = sum / arr.length
+    return medel
+}
+
+function main() {
+    let arr = [1, 7, 1, 0, 5]
+    let result = mean(arr)
+}
+main()*/
+
+/*function reverse(arr) {
+    let arr1 = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+        arr1.push(arr[i])
+    }
+    return arr1
+}
+
+function main() {
+    let arr = [1, 7, 1, 0, 5]
+    let result = reverse(arr)
+}
+main()*/
+
+
+/*function filter(arr, element) {
+    let arr1 = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == element) {
+            arr1.push(arr[i])
+        }
+    }
+    return arr1
+}
+
+function main() {
+    let arr = [1, 7, 1, 0, 5]
+    let element = 1
+    let result = filter(arr, element)
+}
+main()*/
+
+/*function exclude(arr, element) {
+    let arr1 = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] != element) {
+            arr1.push(arr[i])
+        }
+    }
+    return arr1
+}
+
+function main() {
+    let arr = [1, 7, 1, 0, 5]
+    let element = 1
+    let result = exclude(arr, element)
+}
+main()*/
+
+/*
+function unique(arr) {
+    let arr1 = []
+    for (let i = 0; i < arr.length; i++) {
+        let element = arr[i]
+        if (elementInArray(arr, element)) {
+            arr1.push(arr[i])
+        }
+    }
+
+    function elementInArray(arr, element) {
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == element) {
+                return false
+            }
+        }
+        return true
+    }
+    return arr1
+}
+
+function main() {
+    let arr = [1, 7, 1, 0, 5, 0, 5]
+    let result = unique(arr)
+}
+
+main()
+*/
+
+/*
+// split exempel
+let greeting = "hey alla studenter i den gråa fredag"
+let myNewgreeting = greeting.split(" ")
+
+let ownSplit = function (x) {
+    let array = [];
+    let temp = "";
+    let j = 0;
+
+    // har loopar vi igenom varje karaktär i vår string x
+    for (let i = 0; i < x.length; i++) {
+        let el = x[i];
+
+        // kolla om det är ett mellanslag - då skall texten innan pushas in i vår array
+        if (el === " ") {
+            // pusha in textbiten till array
+            array.push(temp);
+            temp = " ";
+        }
+        // Om vi inte matchar mot ett space så betyder det att vi stött på en bokstav
+        if (el !== " ") {
+            // Och denna lägger vi in i vår temporara sträng temp
+            temp = temp + el;
+        }
+    }
+}
+let randomWords = "Heyho let's go";
+ownSplit(randomWords);
+*/
+
+/*
+// reverse strängar i en array
+let arr = ["Gösta", "Kalle", "Olof"];
+function reverseStrings(arr) {
+    // skapa en tom array där vi skall skicka in varje omvända namn
+    let newArr = [];
+    // Loopa igenom varje namn i vår array
+    for (let i = 0; i < arr.length; i++) {
+        let temp = "";
+        let name = arr[i];
+        // Loopa igenom varje bokstav i vårt namn baklänges
+        console.log(name.length);
+        for (let j = name.length - 1; j > 0; j--) {
+            // För varje bokstav lägger vi in den i vår temporära sträng
+            temp = temp + name[j];
+        }
+        newArr.push(temp);
+    }
+}
+// kalla på vår funktion så den faktiskt körs
+reverseStrings(arr)
+*/
+
+/*function join(array, delimiter) {
+    let words = array[0];
+    for (let i = 1; i < array.length; i++) {
+        words =  words + delimiter + array[i];
+    }
+    return words;
+}
+
+function main() {
+    let arr = ["1", "2", "3"]
+    // let arr = ["Kalle", "Olof", "Gösta"]
+    join(arr, " ")
+    // join(arr, ", ")
+}
+
+main()*/
+
+/*
+let str = "kalle,Olof,Gösta,9";
+let delimeter = ",";
+let temp = "";
+function ownSplit(str, del) {
+    let array = [];
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (char === del) {
+            array.push(temp);
+            temp = "";
+        }
+        if (char !== del) {
+            temp = temp + char;
+        }
+    }
+}
+ownSplit(str, delimeter);
+*/
+
+// let arr = ["Kalle", "Olof", "Gösta", "nio"];
+// let delimeter = ",";
+
+// function ownJoin(arr, delimeter) {
+//     let str = "";
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//         str = str + arr[i] + delimeter
+
+//     }
+
+// }
+// ownJoin(arr, delimeter);
---
+++ string-bootcamp/containChar/main.js
@@ -0,0 +1,100 @@
+function repeat(text, repetitions){
+    let newText = text;
+    if(let i = 1; i < repetition; i++) {
+        newText += text;
+    }
+    return newText
+}
+let text = "hello";
+let num = 3;
+repeat(text, 3) // => "hellohellohello"
+repeat("omg", 5) // => "omgomgomgomgomg"
+
+// function containChar(text, character){
+//     let i=0;
+//     while(i < text.length){
+//         if(text[i] === character){
+//             return true;
+        
+            
+//         }
+//         i++;
+//     }
+//     return false;
+// }
+
+// function containsAnywhere(str, otherString){
+//     let j = 0;
+//     for(let i = 0; i < str.length){
+//         if(str[i] == otherString[j]){
+//             i += 1;
+//             j += 1;
+//         }else {
+//             i += 1;
+//         }
+//     }
+//     if(j == otherString.length){
+//         return true;
+//     }else {
+//         return false;
+//     }
+// }
+
+// function contains(str, otherString){
+//     let len = otherString.length;
+//     for(let i = 0; i < str.length; i += 1){
+//         let t=substring(str, i, len - 1 + i);
+//         if(t == otherString){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+// function main(){
+//     let wifi = containChar("superman", "y");
+// }
+// main();
+
+// function indexOfChar(text, character){
+//     for(let i=0; i<text.length; i++){
+//         if(text[i]===character){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+
+// function main(){
+//     let result = indexOfChar("hey", "x");
+// }
+// main();
+
+// function substring(text, start, stop){
+//     let out = "";
+//     for(let i = start; i <= stop; i++){
+//         out += text[i];
+//     }
+//     return out;
+// }
+
+// function remove(text, chars){
+//     let out = "";
+//     let i = 0;
+//     let len = chars.length - 1;
+//     while(i < text.length){
+//         let sub = substring(text, i ,i + len);
+//         if(sub == chars){
+//             i += text[i]
+//         }
+//     }
+// }
+
+function main(){
+    let text = "hello";
+    let num = 3;
+    repeat(text, 3) // => "hellohellohello"
+    repeat("omg", 5) // => "omgomgomgomgomg"
+}
+
+main();
